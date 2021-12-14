d26679499fa5446c7dd6e4c9f865eb92c61b1b33
SuzanneSaleem1 <91476799+SuzanneSaleem1@users.noreply.github.com>
2021-12-14 10:05:56 +0300

    finshing up forogot password feature

--- app/src/main/java/com/example/declutteringapp/LogInFragment.kt
+++ app/src/main/java/com/example/declutteringapp/LogInFragment.kt
@@ -22,12 +22,11 @@ import com.google.firebase.auth.FirebaseUser
 
 class LogInFragment : Fragment() {
 
-    private lateinit var binding : FragmentLogInBinding
+    private lateinit var binding: FragmentLogInBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var firestoreViewModel: FirestoreViewModel
     private lateinit var sharedPreferences: SharedPreferences
     var isRemembered = false
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,116 +69,117 @@ class LogInFragment : Fragment() {
 
             logInAuthentication()
 
-        }}
+        }
+    }
+
+
+    fun logInAuthentication() {
+
+        var email = binding.etEmail.text.toString()
+        var password = binding.etPwd.text.toString()
+
+        when {
+            TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                Toast.makeText(context, "Please Enter Email", Toast.LENGTH_LONG).show()
+            }
+
+            TextUtils.isEmpty(binding.etPwd.text.toString().trim { it <= ' ' }) -> {
+                Toast.makeText(context, "Please Enter Password", Toast.LENGTH_LONG).show()
+            }
+
+            else -> {
+                var userInfo = UserInfoModel()
+                firestoreViewModel.getUserInfo()
+
+                when {
+                    TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
+                        val toastMessageEmail: String =
+                            this@LogInFragment.getResources()
+                                .getString(R.string.please_enter_email)
+
+                        Toast.makeText(
+                            context,
+                            toastMessageEmail,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+
+                    TextUtils.isEmpty(binding.etPwd.text.toString().trim { it <= ' ' }) -> {
+                        val toastMessagePassword: String =
+                            this@LogInFragment.getResources()
+                                .getString(R.string.please_enter_password)
+                        Toast.makeText(context, toastMessagePassword, Toast.LENGTH_LONG).show()
+
+                    }
+                    else -> {
+                        val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
+                        val password: String = binding.etPwd.text.toString().trim { it <= ' ' }
+
+                        // create an instance and create a register with email and password
+                        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener { task ->
+
+                                if (task.isSuccessful) {
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+                                    val toastMessageWelcome: String =
+                                        this@LogInFragment.getResources()
+                                            .getString(R.string.welcome)
+                                    Toast.makeText(
+                                        context,
+                                        "$toastMessageWelcome ${email.toString()}",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+
+                                    val emailPreference: String = email
+                                    val passwordPreference: String = password
+                                    val checked: Boolean = binding.rememberMe.isChecked
+
+                                    val editor: SharedPreferences.Editor =
+                                        sharedPreferences.edit()
+                                    editor.putString("EMAIL", emailPreference)
+                                    editor.putString("PASSWORD", passwordPreference)
+                                    editor.putBoolean("CHECKBOX", checked)
+                                    editor.apply()
+
+                                    val toastMessageInfoSaved: String =
+                                        this@LogInFragment.getResources()
+                                            .getString(R.string.info_saved)
+                                    Toast.makeText(
+                                        context,
+                                        toastMessageInfoSaved,
+                                        Toast.LENGTH_LONG
+                                    ).show()
 
 
 
+                                    firestoreViewModel.readUserData()
 
-         fun logInAuthentication() {
+                                    goToHome()
 
-             var email = binding.etEmail.text.toString()
-             var password = binding.etPwd.text.toString()
+                                } else {
 
-             when {
-                 TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
-                     Toast.makeText(context, "Please Enter Email", Toast.LENGTH_LONG).show()
-                 }
+                                    Toast.makeText(
+                                        context,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_LONG
+                                    ).show()
 
-                 TextUtils.isEmpty(binding.etPwd.text.toString().trim { it <= ' ' }) -> {
-                     Toast.makeText(context, "Please Enter Password", Toast.LENGTH_LONG).show()
-                 }
-
-                 else -> {
-                     var userInfo = UserInfoModel()
-                     firestoreViewModel.getUserInfo()
-
-                     when {
-                         TextUtils.isEmpty(binding.etEmail.text.toString().trim { it <= ' ' }) -> {
-                             val toastMessageEmail: String =
-                                 this@LogInFragment.getResources()
-                                     .getString(R.string.please_enter_email)
-
-                             Toast.makeText(
-                                 context,
-                                 toastMessageEmail,
-                                 Toast.LENGTH_LONG
-                             ).show()
-                         }
-
-                         TextUtils.isEmpty(binding.etPwd.text.toString().trim { it <= ' ' }) -> {
-                             val toastMessagePassword: String =
-                                 this@LogInFragment.getResources()
-                                     .getString(R.string.please_enter_password)
-                             Toast.makeText(context, toastMessagePassword, Toast.LENGTH_LONG).show()
-
-                         }
-                         else -> {
-                             val email: String = binding.etEmail.text.toString().trim { it <= ' ' }
-                             val password: String = binding.etPwd.text.toString().trim { it <= ' ' }
-
-                             // create an instance and create a register with email and password
-                             FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                                 .addOnCompleteListener { task ->
-
-                                     if (task.isSuccessful) {
-                                         val firebaseUser: FirebaseUser = task.result!!.user!!
-                                         val toastMessageWelcome: String =
-                                             this@LogInFragment.getResources()
-                                                 .getString(R.string.welcome)
-                                         Toast.makeText(
-                                             context,
-                                             "$toastMessageWelcome ${email.toString()}",
-                                             Toast.LENGTH_SHORT
-                                         ).show()
+                                }
+                            }
+                    }
+                }
+            }
 
 
-                                         val emailPreference: String = email
-                                         val passwordPreference: String = password
-                                         val checked: Boolean = binding.rememberMe.isChecked
+        }
+    }
 
-                                         val editor: SharedPreferences.Editor =
-                                             sharedPreferences.edit()
-                                         editor.putString("EMAIL", emailPreference)
-                                         editor.putString("PASSWORD", passwordPreference)
-                                         editor.putBoolean("CHECKBOX", checked)
-                                         editor.apply()
+    fun goToHome() {
+        findNavController().navigate(R.id.action_logInFragment_to_startFragment2)
+    }
 
-                                         val toastMessageInfoSaved: String =
-                                             this@LogInFragment.getResources()
-                                                 .getString(R.string.info_saved)
-                                         Toast.makeText(
-                                             context,
-                                             toastMessageInfoSaved,
-                                             Toast.LENGTH_LONG
-                                         ).show()
-
-
-
-                                         firestoreViewModel.readUserData()
-
-                                         goToHome()
-
-                                     } else {
-
-                                     Toast.makeText(
-                                         context,
-                                         task.exception!!.message.toString(),
-                                         Toast.LENGTH_LONG
-                                     ).show()
-
-                                 }
-                         }
-                     }
-                 }
-             }
-
-
-         }}
-fun goToHome() {
-    findNavController().navigate(R.id.action_logInFragment_to_startFragment2)
-}
-
-    fun forgotPasswordDialog(){
+    fun forgotPasswordDialog() {
 
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Forgot Password")
@@ -195,11 +195,15 @@ fun goToHome() {
 
     private fun forgotPassword(userEmail: EditText) {
 
-        if (userEmail.text.toString().isEmpty()){return}
+        auth= FirebaseAuth.getInstance()
+        if (userEmail.text.toString().isEmpty()) {
+            return
+        }
 
 
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(userEmail.text.toString()).matches()) {return}
+        if (!Patterns.EMAIL_ADDRESS.matcher(userEmail.text.toString()).matches()) {
+            return
+        }
 
 
         auth.sendPasswordResetEmail(userEmail.text.toString())
@@ -209,5 +213,6 @@ fun goToHome() {
                 }
             }
 
-}}
 
+    }
+}
